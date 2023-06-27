f03517c30f03299b99ba0038f94acec9edcd63c2
Elena Kim <elena.kim3011@gmail.com>
2023-06-27 11:13:58 +0200

    add app preview; fix default routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -17,6 +17,10 @@ const routes: Routes = [
         path: 'entries',
         component: EntriesListComponent,
       },
+      {
+        path: '**',
+        redirectTo: 'content-types',
+      },
     ],
   },
   {
